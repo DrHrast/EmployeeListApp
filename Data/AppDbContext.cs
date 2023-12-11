5280b8dff3f1136f452f18d621fd1e467e08daca
DrHrast <petar.huljek@gmail.com>
2023-12-11 21:16:20 +0100

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeListApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
